00cfdd40d87f671da7ac734efecd77175398dbd4
R <roulla@Rs-MacBook-Pro.local>
2026-02-20 13:59:30 +0100

    Remove SQLite database for deployment

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Mark better-sqlite3 as external for server-side
-      config.externals = config.externals || []
-      config.externals.push('better-sqlite3')
-    }
-    return config
-  }
+  images: {
+    unoptimized: true
+  },
 }
 
 module.exports = nextConfig
